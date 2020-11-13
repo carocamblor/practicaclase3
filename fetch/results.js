d7e54ef95e61b0b1c51b9e6e2e180278205e4306
Carolina Camblor <ccamblor@udesa.edu.ar>
2020-11-13 12:09:34 -0300

    buscador gifs

---
+++ fetch/results.js
@@ -0,0 +1,39 @@
+window.addEventListener('load', function(){
+    
+    var apiKey = "mfXLwmWUeXozXAJQw84JPoMPgPrTkc84"
+
+    
+    var searchResults = document.querySelector('.search-results ul')
+    var queryString = location.search
+    var queryStringObj = new URLSearchParams(queryString)
+
+    var gifBuscado = queryStringObj.get('q')
+
+    fetch(`https://api.giphy.com/v1/gifs/search?api_key=mfXLwmWUeXozXAJQw84JPoMPgPrTkc84&q=${gifBuscado}&limit=25&offset=0&rating=g&lang=en`)
+
+    .then(function(response) {
+    
+      return response.json()
+    
+    })
+    
+    .then(function(data) { 
+      for (let i = 0; i < data.data.length; i++) {
+        const element = data.data[i];
+        console.log(element)
+          searchResults.innerHTML += `
+          <li>
+            <a href="">${element.title}</a>
+            <img src="${element.images.original.url}" alt="">
+          </li>
+          `
+      }
+    })
+    
+    .catch(function(error) {
+    
+      console.log("Error: " + error);
+    
+    })
+
+})
